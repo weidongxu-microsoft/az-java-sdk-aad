b8bd723a549e6dee7d4322b5efc20e7542da8d40
Weidong Xu <weidxu@microsoft.com>
2022-01-19 16:14:44 +0800

    try avs

--- src/main/java/com/microsoft/azure/aad/Main.java
+++ src/main/java/com/microsoft/azure/aad/Main.java
@@ -5,6 +5,7 @@ import com.azure.core.management.profile.AzureProfile;
 import com.azure.core.util.logging.ClientLogger;
 import com.azure.identity.ManagedIdentityCredentialBuilder;
 import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.avs.AvsManager;
 
 public class Main {
 
@@ -12,9 +13,19 @@ public class Main {
 
     public static void main(String args[]) {
 
-        AzureResourceManager azure = AzureResourceManager.authenticate(new ManagedIdentityCredentialBuilder().build(), new AzureProfile(AzureEnvironment.AZURE)).withDefaultSubscription();
+        AzureProfile profile = new AzureProfile(null, "ec0aa5f7-9e78-40c9-85cd-535c6305b380", AzureEnvironment.AZURE);
 
-        long count = azure.accessManagement().servicePrincipals().list().stream().count();
-        logger.info("Number of service principals: {}", count);
+//        AzureResourceManager azure = AzureResourceManager
+//                .authenticate(new ManagedIdentityCredentialBuilder().build(), profile)
+//                .withDefaultSubscription();
+
+//        long count = azure.accessManagement().servicePrincipals().list().stream().count();
+//        logger.info("Number of service principals: {}", count);
+
+        AvsManager manager = AvsManager
+                .authenticate(new ManagedIdentityCredentialBuilder().build(), profile);
+
+        long count = manager.privateClouds().list().stream().count();
+        logger.info("Number of privateClouds: {}", count);
     }
 }
