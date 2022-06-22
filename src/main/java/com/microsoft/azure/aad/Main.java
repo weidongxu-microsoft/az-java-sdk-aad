96ca64c40643d56602ced250f4b48322bf7b041d
Weidong Xu <weidxu@microsoft.com>
2022-06-22 11:58:27 +0800

    list, flux and repeat

--- src/main/java/com/microsoft/azure/aad/Main.java
+++ src/main/java/com/microsoft/azure/aad/Main.java
@@ -3,9 +3,15 @@ package com.microsoft.azure.aad;
 import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.core.util.logging.ClientLogger;
+import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.identity.ManagedIdentityCredentialBuilder;
 import com.azure.resourcemanager.AzureResourceManager;
-import com.azure.resourcemanager.avs.AvsManager;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -13,19 +19,59 @@ public class Main {
 
     public static void main(String args[]) {
 
-        AzureProfile profile = new AzureProfile(null, "ec0aa5f7-9e78-40c9-85cd-535c6305b380", AzureEnvironment.AZURE);
+        // see https://github.com/Azure/azure-sdk-for-java/tree/main/sdk/resourcemanager#authentication for environment variables
 
+//        AzureProfile profile = new AzureProfile(null, "ec0aa5f7-9e78-40c9-85cd-535c6305b380", AzureEnvironment.AZURE);
+//
 //        AzureResourceManager azure = AzureResourceManager
 //                .authenticate(new ManagedIdentityCredentialBuilder().build(), profile)
 //                .withDefaultSubscription();
 
-//        long count = azure.accessManagement().servicePrincipals().list().stream().count();
-//        logger.info("Number of service principals: {}", count);
+        AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
 
-        AvsManager manager = AvsManager
-                .authenticate(new ManagedIdentityCredentialBuilder().build(), profile);
+        AzureResourceManager azureResourceManager = AzureResourceManager
+                .authenticate(new DefaultAzureCredentialBuilder().build(), profile)
+                .withDefaultSubscription();
 
-        long count = manager.privateClouds().list().stream().count();
-        logger.info("Number of privateClouds: {}", count);
+        List<Flux<?>> fluxList = Arrays.asList(
+                azureResourceManager.storageAccounts().listAsync(),
+                azureResourceManager.networkSecurityGroups().listAsync(),
+                azureResourceManager.applicationSecurityGroups().listAsync(),
+                azureResourceManager.routeTables().listAsync(),
+                azureResourceManager.networks().listAsync(),
+                azureResourceManager.localNetworkGateways().listAsync(),
+                azureResourceManager.virtualNetworkGateways().listAsync(),
+                azureResourceManager.networkInterfaces().listAsync(),
+                azureResourceManager.virtualMachines().listAsync(),
+                azureResourceManager.applicationGateways().listAsync(),
+                azureResourceManager.loadBalancers().listAsync(),
+                // I don't have permission on this
+//                azureResourceManager.accessManagement().activeDirectoryUsers().listAsync(),
+                azureResourceManager.policyAssignments().listAsync(),
+                azureResourceManager.publicIpAddresses().listAsync(),
+                azureResourceManager.networks().manager().serviceClient().getAzureFirewalls().listAsync(),
+                azureResourceManager.networks().manager().serviceClient().getIpGroups().listAsync(),
+                azureResourceManager.sqlServers().manager().serviceClient().getManagedInstances().listAsync(),
+                azureResourceManager.sqlServers().listAsync()
+                        .flatMap(it -> azureResourceManager.sqlServers().firewallRules().listBySqlServerAsync(it)),
+                azureResourceManager.cosmosDBAccounts().listAsync()
+        );
+
+        var publisherList = fluxList.stream()
+                // I don't see why collectList which partly defeats the purpose of Flux, but use it anyway
+                .map(it -> it.collectList().flatMapIterable(it1 -> it1))
+                .collect(Collectors.toList());
+
+        var retrieve = Flux.merge(publisherList.toArray(new Flux[0]));
+
+//        retrieve.blockLast();
+
+        Flux.just(1, 2, 3, 4, 5, 6).flatMap(n -> {
+            Mono<List<Object>> asyncList = retrieve.collectList();
+            return asyncList
+                    .doOnNext(it -> System.out.println("size " + it.size()))
+                    .doOnSubscribe(it -> System.out.println("start " + n));
+//                    .subscribeOn(workScheduler)
+        }).repeat(100).blockLast();
     }
 }
