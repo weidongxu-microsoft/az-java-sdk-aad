739cf0893a7c44730da86bf1a663708012d21b32
Weidong Xu <weidxu@microsoft.com>
2024-07-09 15:59:42 +0800

    use  PolicyDefinition

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Sample project for Azure Active Directory via Azure Java Management SDK
+
+```
+mvn clean package
+mvn exec:java
+```
--- src/main/java/com/microsoft/azure/aad/Main.java
+++ src/main/java/com/microsoft/azure/aad/Main.java
@@ -6,10 +6,13 @@ import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.core.util.logging.ClientLogger;
 import com.azure.identity.DefaultAzureCredentialBuilder;
-import com.azure.resourcemanager.AzureResourceManager;
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
-import reactor.core.observability.micrometer.Micrometer;
+import com.azure.resourcemanager.resources.ResourceManager;
+import com.azure.resourcemanager.resources.models.ParameterDefinitionsValue;
+import com.azure.resourcemanager.resources.models.ParameterType;
+import com.azure.resourcemanager.resources.models.PolicyDefinition;
+import com.azure.resourcemanager.resources.models.PolicyType;
+
+import java.util.Map;
 
 public class Main {
 
@@ -17,25 +20,40 @@ public class Main {
 
     public static void main(String ...args) {
 
-        MeterRegistry meterRegistry = new LoggingMeterRegistry();
-
         // see https://github.com/Azure/azure-sdk-for-java/tree/main/sdk/resourcemanager#authentication for environment variables
 
+        // define env variable AZURE_SUBSCRIPTION_ID for subscription ID
+
+        // DO NOT CODE SECRET
         TokenCredential credential = new DefaultAzureCredentialBuilder().build();
         AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
 
-        AzureResourceManager azureResourceManager = AzureResourceManager
-                .configure()
-                .withLogLevel(HttpLogDetailLevel.BASIC)
+        final ResourceManager resourceManager = ResourceManager.configure()
+                .withLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS)
                 .authenticate(credential, profile)
                 .withDefaultSubscription();
 
-        var rg = azureResourceManager.resourceGroups().getByNameAsync("rg-weidxu")
-                .name("getByNameAsync")
-                .tap(Micrometer.metrics(meterRegistry))
-                .log()
-                .block();
+        final PolicyDefinition.DefinitionStages.WithCreate policyDefinition = resourceManager.policyDefinitions()
+                .define("policyName")
+                .withPolicyRuleJson("{\"if\":{\"not\":{\"field\":\"name\",\"like\":\"[concat(parameters('prefix'),'*',parameters('suffix'))]\"}},\"then\":{\"effect\":\"deny\"}}")
+                .withDisplayName("displayName")
+                .withDescription("description")
+                .withPolicyType(PolicyType.CUSTOM)
+                .withMode("All")
+                .withMetadata(Map.of("category", "Compute"));
 
-        meterRegistry.close();
+        policyDefinition.withParameter("prefix", ParameterType.STRING, "dept");
+
+        policyDefinition.withParameter("suffix", new ParameterDefinitionsValue()
+                .withType(ParameterType.STRING)
+                .withDefaultValue("-US"));
+
+        PolicyDefinition policyDefinitionCreated = policyDefinition.create();
+
+        LOGGER.info("PolicyDefinition created, rule: " + policyDefinitionCreated.policyRule().toString());
+
+        resourceManager.policyDefinitions().deleteByName("policyName");
+
+        LOGGER.info("PolicyDefinition deleted");
     }
 }
